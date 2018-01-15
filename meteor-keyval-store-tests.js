83533c8bac9694aef33be2ce9488c8bc2adf3253
liberation <o.dolgarev@gmail.com>
2018-01-15 09:47:22 +0200

    add version 0.0.1

---
+++ meteor-keyval-store-tests.js
@@ -0,0 +1,50 @@
+// Import Tinytest from the tinytest Meteor package.
+import { Tinytest } from 'meteor/tinytest'
+import KeyValStore from 'meteor/liberation:keyval-store'
+
+const store = new (KeyValStore())('__dummy_kv_store__')
+
+Tinytest.add('keyval-store - setSet', function (test) {
+  test.isUndefined(store.setItem('test1', 123))
+})
+
+Tinytest.add('keyval-store - getItem', function (test) {
+  test.equal(store.getItem('test1'), 123)
+})
+
+Tinytest.add('keyval-store - incItem', function (test) {
+  test.equal(store.incItem('test1'), 124)
+  test.equal(store.incItem('test1', 6), 130)
+})
+
+Tinytest.add('keyval-store - decItem', function (test) {
+  test.equal(store.decItem('test1'), 129)
+  test.equal(store.decItem('test1', 6), 123)
+})
+
+Tinytest.add('keyval-store - deleteItem', function (test) {
+  test.equal(store.deleteItem('test1'), 123)
+  test.isUndefined(store.getItem('test1'))
+})
+
+Tinytest.add('keyval-store - getItem with default value', function (test) {
+  test.isNull(store.getItem('test1', null))
+  test.equal(store.getItem('test1', 0), 0)
+  test.equal(store.getItem('test1', false), false)
+  test.equal(store.getItem('test1', ''), '')
+})
+
+Tinytest.add('keyval-store - incCounter', function (test) {
+  test.isUndefined(store.setItem('test2', -1))
+  test.isUndefined(store.incCounter('test2'))
+  test.equal(store.setItem('test2', 0), -1)
+  test.equal(store.incCounter('test2'), 1)
+})
+
+Tinytest.add('keyval-store - decCounter', function (test) {
+  test.isUndefined(store.setItem('test3', -1))
+  test.isUndefined(store.decCounter('test3'))
+  test.equal(store.setItem('test3', 1), -1)
+  test.equal(store.decCounter('test3'), 0)
+  test.isUndefined(store.decCounter('test3'))
+})
---
+++ meteor-keyval-store.js
@@ -0,0 +1,81 @@
+import { Meteor } from 'meteor/meteor'
+import { Mongo } from 'meteor/mongo'
+
+const UNDEF = void 0
+
+export default (superclass = Mongo.Collection) => class extends superclass {
+  constructor (...args) {
+    super(...args)
+    this._coll = this.rawCollection()
+  }
+
+  getItem (key, defaultVal) {
+    const res = this.findOne(key, { fields: { val: 1 } })
+    return res === UNDEF
+      ? defaultVal
+      : res.val
+  }
+
+  setItem (key, val) {
+    const res = Meteor.wrapAsync(callback => {
+      this._coll.findOneAndUpdate({
+        _id: key
+      }, {
+        $set: { val }
+      }, {
+        upsert: true
+      }, callback)
+    })()
+    return res.value === null ? UNDEF : res.value.val
+  }
+
+  deleteItem (key) {
+    const res = Meteor.wrapAsync(callback => {
+      this._coll.findOneAndDelete({ _id: key }, {}, callback)
+    })()
+    return res.value === null ? UNDEF : res.value.val
+  }
+
+  hasItem (key) {
+    return !!this.findOne(key)
+  }
+
+  incCounter (key, step = 1) {
+    const res = Meteor.wrapAsync(callback => {
+      this._coll.findOneAndUpdate({
+        _id: key,
+        val: step > 0 ? { $gte: 0 } : { $gt: 0 }
+      }, {
+        $inc: { val: step }
+      }, {
+        projection: { val: 1 },
+        returnOriginal: false
+      }, callback)
+    })()
+    return res.value === null ? UNDEF : res.value.val
+  }
+
+  incItem (key, step = 1) {
+    const res = Meteor.wrapAsync(callback => {
+      this._coll.findOneAndUpdate({
+        _id: key
+      }, {
+        $inc: { val: step }
+      }, {
+        projection: { val: 1 },
+        returnOriginal: false
+      }, callback)
+    })()
+    return res.value === null ? UNDEF : res.value.val
+  }
+
+  decCounter (key, step = -1) {
+    step = step < 0 ? step : -Math.abs(step)
+    return this.incCounter(key, step)
+  }
+
+  decItem (key, step = -1) {
+    step = step < 0 ? step : -Math.abs(step)
+    return this.incItem(key, step)
+  }
+}
